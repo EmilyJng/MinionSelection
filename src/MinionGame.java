29797ce42b4949abc9270388b38695cfbb723142
Emily <emjayjung@outlook.de>
2020-12-10 17:25:08 +0100

    Initial Commit

---
+++ src/MinionGame.java
@@ -0,0 +1,16 @@
+
+public class MinionGame {
+	public static void main(String[] args) {
+
+		double starterSelection;
+
+		starterSelection = Math.random();
+		if (starterSelection >= 0.5) {
+			System.out.print("Computer beginnt");
+		} else {
+			System.out.print("Spieler beginnt");
+		}
+
+		
+	}
+}
