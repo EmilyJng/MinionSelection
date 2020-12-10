028ebb0146a372846893af639e79a7faf8393ded
Emily <emjayjung@outlook.de>
2020-12-10 18:17:03 +0100

    Algorithmus zur Reihenfolge

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -3,14 +3,29 @@ public class MinionGame {
 	public static void main(String[] args) {
 
 		double starterSelection;
+		String order = "";
+		boolean controlNorbert = false;
 
-		starterSelection = Math.random();
-		if (starterSelection >= 0.5) {
-			System.out.print("Computer beginnt");
-		} else {
-			System.out.print("Spieler beginnt");
+		for (int i = 0; i < 11; i++) {
+			if (controlNorbert == false) {
+				double probability = Math.random();
+				if (probability <= 0.5) {
+					order += "M";
+				} else {
+					order += "N";
+					controlNorbert = true;
+				}
+			} else {
+				order += "M";
+			}
 		}
 
-		
+		/*
+		 * starterSelection=Math.random();if(starterSelection>=0.5)
+		 * 
+		 * { System.out.print("Computer beginnt"); }else {
+		 * System.out.print("Spieler beginnt"); }
+		 */
+
 	}
 }
