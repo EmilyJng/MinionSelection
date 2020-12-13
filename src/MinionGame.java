6b5af661519164b35bb9fe2336aa41c417f49674
Emily <emjayjung@outlook.de>
2020-12-13 17:07:36 +0100

    while bedingung um eingabe 1-3 Minions wählen + links oder rechts

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -10,6 +10,7 @@ public class MinionGame {
 		int starter;
 		int choice = 0;
 		String choosenSide;
+		boolean choiceValid = false;
 
 		while (controlNorbert == false) {
 			order = "";
@@ -50,18 +51,31 @@ public class MinionGame {
 		if (starter == 0) {
 			System.out.print("Computer wählt");
 		} else {
+
+		}
+		while (choiceValid == false) {
+			choice = 0;
+
 			System.out.print("Wähle zwischen 1 und 3 Minions");
 			choice = StaticScanner.nextInt();
+
 			if (choice == 1 || choice == 2 || choice == 3) {
 				System.out.print("Von Links oder Rechts? (Eingabe 'l' für Links, Eingabe 'r' für Rechts");
 				String choiceTemp = StaticScanner.nextString();
-					if (choiceTemp.equals("r") || choiceTemp.equals("l")) {
-						choosenSide = choiceTemp;
-					} else {
-						System.out.print("Du musst l oder r eingeben um die Seite zu wählen");
-					}
+
+				if (choiceTemp.equals("r") || choiceTemp.equals("l")) {
+					choosenSide = choiceTemp;
+					choiceValid = true;
+
+				} else {
+					System.out.print("Du musst l oder r eingeben um die Seite zu wählen");
+					choiceValid = false;
+				}
+
 			} else {
 				System.out.print("Du musst zwischen 1 und 3 Minions wählen!");
+				choiceValid = false;
+
 			}
 		}
 
