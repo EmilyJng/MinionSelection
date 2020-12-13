d48bc4fbd83918cf85a9121cd20d3ed6b964b9bb
Emily <emjayjung@outlook.de>
2020-12-13 19:35:28 +0100

    Zuerst Seite wählen, dann Zahl + Kommentar

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -11,7 +11,6 @@ public class MinionGame {
 		int choice = 0;
 		String choosenSide;
 		boolean choiceValid = false;
-		
 
 		while (controlNorbert == false) {
 			order = "";
@@ -36,6 +35,10 @@ public class MinionGame {
 		System.out.println("Norbert steht an " + positionNorbert + ". Stelle.");
 		System.out.println("Es stehen/steht " + (minionsRight) + " rechts.");
 		System.out.println("Es stehen/ steht " + (minionsLeft) + " links.");
+		
+		/**
+		 * Auswahl Spieler oder Computer
+		 */
 
 		starterSelection = Math.random();
 		if (starterSelection >= 0.5)
@@ -50,37 +53,47 @@ public class MinionGame {
 		}
 
 		if (starter == 0) {
+			/**
+			 * Zug Computer
+			 */
 			System.out.println("Computer wählt");
 		} else {
-
+			/**
+			 * Zug Mensch
+			 */
 			while (choiceValid == false) {
 				choice = 0;
-
-				System.out.println("Wähle zwischen 1 und 3 Minions");
-				choice = StaticScanner.nextInt();
-
-				if (choice == 1 || choice == 2 || choice == 3) {
-					System.out.println("Von Links oder Rechts? (Eingabe 'l' für Links, Eingabe 'r' für Rechts");
-					String choiceTemp = StaticScanner.nextString();
-
-					while (true) {
-
-						if (choiceTemp.equals("r") && minionsRight >= choice
-								|| choiceTemp.equals("l") && minionsLeft >= choice) {
-							choosenSide = choiceTemp;
-							choiceValid = true;
-							break;
+				System.out.println("Von welcher Seite willst du ziehen?(Eingabe 'l' für Links, Eingabe 'r' für Rechts");
+				String choiceTemp = StaticScanner.nextString().toLowerCase();
+				while (true) {
+					
+					if (choiceTemp.equals("r") || choiceTemp.equals("l")) {
+						choosenSide = choiceTemp;
+						System.out.println("Wähle zwischen 1 und 3 Minions");
+						choice = StaticScanner.nextInt();
+						
+						if (choice == 1 || choice == 2 || choice == 3) {
+							if(choiceTemp.equals("r") && minionsRight >= choice || choiceTemp.equals("l") && minionsLeft >= choice) {
+								String side = "";
+								if(choiceTemp.equals("l")) {
+									side = "Links";
+								}
+								else if(choiceTemp.equals("r")) {
+									side = "Rechts";
+								}
+								System.out.println("Du ziehst " + choice + " Minions von " + side);
+								choiceValid = true;
+								break;
+							}
 
 						} else {
-							System.out.println("Du musst l oder r eingeben um die Seite zu wählen");
-
+							System.out.println("Du musst zwischen 1 und 3 Minions wählen!");
+							choiceValid = false;
 						}
+
+					} else {
+						System.out.println("Du musst l oder r eingeben um die Seite zu wählen");
 					}
-
-				} else {
-					System.out.println("Du musst zwischen 1 und 3 Minions wählen!");
-					choiceValid = false;
-
 				}
 			}
 		}
