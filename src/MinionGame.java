4ef0ceb238baa4cc865fd666ef631177939421b1
Emily <emjayjung@outlook.de>
2020-12-13 18:51:27 +0100

    Korrektur der wahl l und r durch while schleife mit break

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -11,6 +11,7 @@ public class MinionGame {
 		int choice = 0;
 		String choosenSide;
 		boolean choiceValid = false;
+		
 
 		while (controlNorbert == false) {
 			order = "";
@@ -40,44 +41,48 @@ public class MinionGame {
 		if (starterSelection >= 0.5)
 
 		{
-			System.out.print("Computer beginnt");
+			System.out.println("Computer beginnt");
 			starter = 0;
 
 		} else {
-			System.out.print("Spieler beginnt");
+			System.out.println("Spieler beginnt");
 			starter = 1;
 		}
 
 		if (starter == 0) {
-			System.out.print("Computer wählt");
+			System.out.println("Computer wählt");
 		} else {
 
-		}
-		while (choiceValid == false) {
-			choice = 0;
+			while (choiceValid == false) {
+				choice = 0;
 
-			System.out.print("Wähle zwischen 1 und 3 Minions");
-			choice = StaticScanner.nextInt();
+				System.out.println("Wähle zwischen 1 und 3 Minions");
+				choice = StaticScanner.nextInt();
 
-			if (choice == 1 || choice == 2 || choice == 3) {
-				System.out.print("Von Links oder Rechts? (Eingabe 'l' für Links, Eingabe 'r' für Rechts");
-				String choiceTemp = StaticScanner.nextString();
+				if (choice == 1 || choice == 2 || choice == 3) {
+					System.out.println("Von Links oder Rechts? (Eingabe 'l' für Links, Eingabe 'r' für Rechts");
+					String choiceTemp = StaticScanner.nextString();
 
-				if (choiceTemp.equals("r") || choiceTemp.equals("l")) {
-					choosenSide = choiceTemp;
-					choiceValid = true;
+					while (true) {
+
+						if (choiceTemp.equals("r") && minionsRight >= choice
+								|| choiceTemp.equals("l") && minionsLeft >= choice) {
+							choosenSide = choiceTemp;
+							choiceValid = true;
+							break;
+
+						} else {
+							System.out.println("Du musst l oder r eingeben um die Seite zu wählen");
+
+						}
+					}
 
 				} else {
-					System.out.print("Du musst l oder r eingeben um die Seite zu wählen");
+					System.out.println("Du musst zwischen 1 und 3 Minions wählen!");
 					choiceValid = false;
+
 				}
-
-			} else {
-				System.out.print("Du musst zwischen 1 und 3 Minions wählen!");
-				choiceValid = false;
-
 			}
 		}
-
 	}
 }
