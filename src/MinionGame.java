a4e790266151d7b31f45f1bed0b21e030c7b294d
Emily <emjayjung@outlook.de>
2020-12-13 15:53:00 +0100

    funktionierender static Scanner mit while, starter auswahl

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -1,34 +1,56 @@
-
 public class MinionGame {
 	public static void main(String[] args) {
 
 		double starterSelection;
 		String order = "";
 		boolean controlNorbert = false;
+		int positionNorbert = 0;
+		int minionsLeft = 0;
+		int minionsRight = 0;
+		int starter;
+		int choice = 0;
 
 		while (controlNorbert == false) {
+			order = "";
 			for (int i = 0; i < 11; i++) {
 				if (controlNorbert == false) {
-					double probability = Math.random();
-					if (probability <= 0.5) {
-						order += "M";
+					double probability = Math.random() * i;
+					if (probability < 0.85) {
+						order += " M ";
 					} else {
-						order += "N";
+						order += " O ";
 						controlNorbert = true;
+						positionNorbert = i + 1;
+						minionsLeft = positionNorbert - 1;
+						minionsRight = 11 - positionNorbert;
 					}
 				} else {
-					order += "M";
+					order += " M ";
 				}
 			}
 		}
 		System.out.println(order);
+		System.out.println("Norbert steht an " + positionNorbert + ". Stelle.");
+		System.out.println("Es stehen " + (minionsRight) + " rechts.");
+		System.out.println("Es stehen " + (minionsLeft) + " links.");
 
-		/*
-		 * starterSelection=Math.random();if(starterSelection>=0.5)
-		 * 
-		 * { System.out.print("Computer beginnt"); }else {
-		 * System.out.print("Spieler beginnt"); }
-		 */
+		starterSelection = Math.random();
+		if (starterSelection >= 0.5)
+
+		{
+			System.out.print("Computer beginnt");
+			starter = 0;
+
+		} else {
+			System.out.print("Spieler beginnt");
+			starter = 1;
+		}
+
+		if (starter == 0) {
+			System.out.print("Computer wählt");
+		} else {
+			choice = StaticScanner.nextInt();
+		}
 
 	}
 }
