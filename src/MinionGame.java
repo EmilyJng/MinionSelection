b64e75975bea179a0512d9c3abce99f46f5538c0
Emily <emjayjung@outlook.de>
2020-12-23 21:34:34 +0100

    finale version mit kommentaren

--- src/MinionGame.java
+++ src/MinionGame.java
@@ -37,7 +37,7 @@ public class MinionGame {
 				+ "7. Wer Norbert in seinem Team hat, hat verloren und bekommt den Job als Direktor nicht.\n" + "");
 
 		System.out.println(
-				"-----------------------------GAME START-------------------------------------------------------\n");
+				"--------------------------GAME START----------------------------------------------------------\n");
 		// Aufrufen der Methode um zufällig zu bestimmen, wer anfängt
 		int starter = getBeginner();
 		// Ausgabe der Position von Norbert,
@@ -56,23 +56,35 @@ public class MinionGame {
 		// Beginner feststellen durch Aufrufen der Methode
 
 		// Schleife für den Spielverlauf
+		// Sie läuft solange, wie noch Minions vorhanden sind und keiner Norbert gezogen hat
+		// Norbert gezogen würde gameOver auf true setzen
 
 		while (((minionsLeft > 0) || (minionsRight > 0)) && gameOver == false) {
+			// Spielerzug
 			if (starter == 1) {
 				int amount = 0;
 				char side = '-';
 				boolean sideInput = true;
+				// Solange sideInput true ist, läuft die zweite Schleife
 				while (sideInput) {
 					boolean minionAmountInput = true;
 					System.out.println("Wähle eine Seite von der du ziehen möchtest. 'r' für Rechts, 'l' für Links");
 					side = StaticScanner.nextChar();
 					if (side == 'r' || side == 'l') {
+						//solange minionAmountInput true ist, läuft die dritte Schleife
 						while (minionAmountInput) {
 							System.out.println("Wähle zwischen 1 und 3 Minions! Wie viele möchtest du ziehen?");
 							amount = StaticScanner.nextInt();
+							// Da nur zwischen 1 und 3 Minions gewählt werden darf, muss amount größer als 0 und kleiner als 4 sein
+							// Falls die Ausgewählte Zahl größer ist, landet man unten beim else und wird gefragt ob man die Seite neu wählen will
 							if ((amount > 0 && amount < 4)) {
+								// Abgleich der Auswahl mit den noch vorhandenen Minions
 								if (side == 'r' && amount <= minionsRight || side == 'l' && amount <= minionsLeft) {
 									if (side == 'r') {
+										// Aktualisieren der Minions durch bisherige Anzahl - die ausgewählten Minions
+										// Die "-" werden durch minionsRightTaken dargestellt
+										// Die Aktualisierung ist wie bei den normalen Minions auf der Seite
+										// Um am Ende sagen zu können, wie viele Minion jeder hat, werden diese über minionTotalPlayer gespeichert
 										minionsRight = minionsRight - amount;
 										minionsRightTaken = minionsRightTaken + amount;
 										minionsTotalPlayer = minionsTotalPlayer + amount;
@@ -82,10 +94,15 @@ public class MinionGame {
 										minionsLeftTaken = minionsLeftTaken + amount;
 										minionsTotalPlayer = minionsTotalPlayer + amount;
 									}
+									// egal ob links oder rechts wird starter als 0 gestellt, damit die else if Bedingung in der While Schleife erfüllt ist
+									// Der Computer ist somit als nächstes dran
+									// Durch sideInput false kommt man aus der dritten Schleife heraus
 									sideInput = false;
 									starter = 0;
 									break;
 								} else {
+									// Mit der Eingabe würde man Norbert ziehen, deshalb Nachfrage
+									// je nach Antwort neue Wahl der Seite oder Norbert ziehen hasNorbert wird 'p'-> Spiel vorbei
 									System.out.println("Möchtest du Norbert ziehen? (j/n)");
 									char choice = StaticScanner.nextChar();
 									if (choice == 'j') {
@@ -97,6 +114,8 @@ public class MinionGame {
 									}
 								}
 							} else {
+								// bei falscher Eingabe neue Eingabemöglichkeit durch Frage nach der Seite
+								// break um wieder zur Wahl der Seite zu gelangen
 								System.out.println(
 										"Es ist nicht möglich die gewählte Anzahl an Minions zu ziehen. Möchtest du eine andere Seite wählen? (j/n)");
 								char choice = StaticScanner.nextChar();
@@ -107,30 +126,41 @@ public class MinionGame {
 						}
 					}
 				}
+				// um nicht nur r oder l in der Ausgabe zu haben, umwandeln in String choosenSide
 				if (side == 'l') {
 					choosenSide = "links";
 				} else {
 					choosenSide = "rechts";
 				}
+				
+				// Ausgabe der gewählten Minions + Seite
+				// Ausgabe der aktuellen Minions im eigenen Team
 				System.out.println("Du ziehst " + amount + " Minions von " + choosenSide + "\n");
 				System.out.println("Aktuell hast du " + minionsTotalPlayer + " Minions in deinem Team \n");
 				System.out.println("- Spieler Zug beendet - \n");
+				// Strich zur Übersichtlichkeit, Abgrenzung der Züge von Spieler und Computer
 				System.out.println("________________________________________________________________________\n");
+				// Ausgabe der aktuellen Minionreihe durch Aufrufen der Methode mit den aktuellen Werten
 				System.out.println("Folgende Minions sind noch übrig:");
 				printOrder(positionNorbert, minionsLeft, minionsRight, minionsLeftTaken, minionsRightTaken);
 
+				// Wenn nur links und rechts keine Minions mehr stehen, also nur Norbert übrig, verliert der Computer, der als nächstes ziehen muss
+				// hasNorbert wird 'c' zugewiesen um es am Ende ausgeben zu können
 				if ((minionsLeft == 0) && (minionsRight == 0)) {
 					System.out.println(
 							"Nur noch Norbert ist übrig. Der Computer muss bei seinem nächsten Zug Norbert ziehen. \n");
 					hasNorbert = 'c';
+					// plus 1, da Norbert mit ins andere Team kommt
 					minionsTotalComputer = minionsTotalComputer + 1;
 					System.out.println("Der Spieler gewinnt! \n");
+					// true um aus der Schleife des Spielverlaufs zu gelangen
 					gameOver = true;
 				}
-
+				// Computerzug
 			} else if (starter == 0) {
 				int amount = 0;
 				char side = '-';
+				// sofern auf einer Seite nur noch 1,2 oder 3 Minions stehen, wählt der Computer diese aus
 				if (minionsLeft == 1 || minionsLeft == 2 || minionsLeft == 3) {
 					// hier wird entsprechend die 3,2 oder 1 übrigen von links ausgewählt
 					amount = minionsLeft;
@@ -142,6 +172,7 @@ public class MinionGame {
 					side = 'r';
 
 				} else {
+					// Wenn die Bedingung oben nicht zutrifft, wählt der Computer zufällig Seite und Anzahl der Minions aus
 					double probabilitySide = Math.random();
 					double probabilityAmount = Math.random();
 					if (probabilitySide > 0.5) {
@@ -150,6 +181,9 @@ public class MinionGame {
 
 						if (probabilityAmount <= 0.3) {
 							amount = 1;
+							// Sofern auf der linken Seite genügend Minions stehen, wird dort 1 gewählt
+							// Sind dort nicht genügend Minions, wird automatisch die Anzahl auf der rechten Seite gewählt
+							// Das Gleiche gilt für amount 2 und 3
 							if (minionsLeft >= 1) {
 
 								side = 'l';
@@ -171,7 +205,6 @@ public class MinionGame {
 
 							}
 
-							// System.out.println("Ich nehme 2 Minions von Links");
 						} else if (probabilityAmount > 0.6 && probabilityAmount < 1) {
 							amount = 3;
 							if (minionsLeft >= 3) {
@@ -183,18 +216,17 @@ public class MinionGame {
 								side = 'r';
 
 							}
-						//  Der Computer nimmt  3 Minions von Links");
 						}
 					} else {
 
 						// rechts
+						// Gleiches verfahren wie bei links, nur umgekehrt
 
 						if (probabilityAmount <= 0.3) {
 							amount = 1;
 							if (minionsRight >= 1) {
 
 								side = 'r';
-							//  Der Computer nimmt 1 Minion von Rechts
 
 							} else {
 
@@ -212,7 +244,7 @@ public class MinionGame {
 
 								side = 'l';
 							}
-							// Der Computer nimmt 2 Minions von Rechts");
+
 						} else if (probabilityAmount > 0.6 && probabilityAmount < 1) {
 							amount = 3;
 							if (minionsRight >= 3) {
@@ -224,7 +256,6 @@ public class MinionGame {
 								side = 'l';
 
 							}
-							// System.out.println("Ich nehme 3 Minions von Rechts");
 
 						}
 
@@ -252,28 +283,27 @@ public class MinionGame {
 				System.out.println("________________________________________________________________________\n");
 				System.out.println("Folgende Minions sind noch übrig:");
 				printOrder(positionNorbert, minionsLeft, minionsRight, minionsLeftTaken, minionsRightTaken);
-
+				// Starter 1 zuweisen damit der Spieler als nächstes dran ist
 				starter = 1;
-
+				// falls keine Minions mehr links und rechts stehen, ist klar, dass der Spieler Norbert ziehen muss
 				if ((minionsLeft == 0) && (minionsRight == 0)) {
 					System.out.println(
 							"Nur noch Norbert ist übrig. Beim nächsten Zug muss der Spieler Norbert ziehen! \n");
 					System.out.println("Der Computer gewinnt! \n");
+					// zuweisung von hasNorbert für den Spieler
 					hasNorbert = 'p';
 					minionsTotalPlayer = minionsTotalPlayer + 1;
 					break;
 				}
-			} else {
-				System.out.println("Fehler");
-
 			}
-
+			// je nachdem was hasNorbert zugewiesen bekommen hat, Ausgabe für wer Norbert gezogen hat 
 			if (hasNorbert == 'c') {
 				System.out.println("Der Computer hat Norbert gezogen!");
 			} else if (hasNorbert == 'p') {
 				System.out.println("Der Spieler hat Norbert gezogen! \n");
 			}
 		}
+		// Punkte zur visuellen Abgrenzung, dass das Spiel vorbei ist. Anschließende Ausgabe der Minions im jeweiligen Team
 		System.out.println(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n");
 
 		System.out.println("Der Computer hat insgesamt " + minionsTotalComputer + " Minions in seinem Team. \n");
@@ -291,7 +321,8 @@ public class MinionGame {
 
 	public static int getPositionNorbert() {
 		int positionNorbert = 1;
-
+		// Damit Norbert nicht ganz am Rand steht, begrenzung auf größer 3 und kleiner 9
+		// Falls der Wert zufällig außerhalb des bereiches ist, läuft die Schleife erneut durch
 		while (positionNorbert < 3 || positionNorbert > 9) {
 
 			positionNorbert = (int) (Math.random() * 10);
@@ -301,7 +332,7 @@ public class MinionGame {
 
 	}
 
-	// Methode für Anzahl Minions links erhalten
+	// Methode für den Anfangswert von den Minions links
 
 	public static int getMinionsLeft(int positionNorbert) {
 		int minionsLeft;
@@ -312,7 +343,7 @@ public class MinionGame {
 
 	}
 
-	// Methode für Minions rechts erhalten
+	//Methode für den Anfangswert von den Minions rechts
 
 	public static int getMinionsRight(int positionNorbert) {
 		int minionsRight;
@@ -323,7 +354,7 @@ public class MinionGame {
 
 	}
 
-	// Methode für Reihenfolge als String erstellen und oben aufrufen bzw anzeigen
+	// Methode für Reihenfolge aus den einzelnen Teilen erstellen und oben aufrufen bzw als String anzeigen
 	// können
 
 	public static void printOrder(int positionNorbert, int minionsLeft, int minionsRight, int minionsLeftTaken,
@@ -358,13 +389,14 @@ public class MinionGame {
 
 		{
 			System.out.println("Der Computer beginnt diese Runde!");
+			// oben in der Schleife wird durch 0 und 1 gesagt, wer dran ist
 			starter = 0;
 
 		} else {
 			System.out.println("Der Spieler beginnt diese Runde!");
 			starter = 1;
 		}
-		System.out.println("");
+		System.out.println("\n");
 		return starter;
 	}
 
